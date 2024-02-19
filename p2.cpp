6c18643a1e486156151b646ebaa56030daff374d
Gaurav Shrivastava <gg283421@gmail.com>
2024-02-18 19:51:58 -0600

    edit 1

--- p2.cpp
+++ p2.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-   
-
+    cout<<"Mergesort and Quicksort for part 1";
+    cout<<"File handling for part 2";
+    cout<<"learn linkedlist before 27th";
     return 0;
 }
