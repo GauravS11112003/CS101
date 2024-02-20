8fcd9f92f3972bbfc863fb5eee7724a843af7129
Gaurav Shrivastava <gg283421@gmail.com>
2024-02-19 22:22:11 -0600

    final edit

--- p2.cpp
+++ p2.cpp
@@ -1,146 +1,288 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
-#include <vector>
-#include <algorithm>
-
+#include <fstream>
 using namespace std;
 
-// Mergesort algorithm for integers
-void merge(vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    
-    vector<int> L(n1);
-    vector<int> R(n2);
+int SegregateArray(int data[], int start, int end) {
+   int low;
+   int high;
+   int mid;
+   int pivot;
+   int temp;
+   bool sorted;
+   
+   mid = start + (end - start) / 2;
+   pivot = data[mid];
+   
+   sorted = false;
+   low = start;
+   high = end;
+   
+   while (!sorted) {
+      while (data[low] < pivot) {
+         ++low;
+      }
+      
+      while (pivot < data[high]) {
+         --high;
+      }
+      
+      if (low >= high) {
+         sorted = true;
+      }
+      else {
+         temp = data[low];
+         data[low] = data[high];
+         data[high] = temp;
+         
+         ++low;
+         --high;
+      }
+   }
+   
+   return high;
+}
 
-    for (int i = 0; i < n1; ++i)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; ++j)
-        R[j] = arr[mid + 1 + j];
+void SortArray(int data[], int start, int end) {
+   int partition;
+   
+   if (start >= end) {
+      return;
+   }
+   
+   partition = SegregateArray(data, start, end);
+   
+   SortArray(data, start, partition);
+   SortArray(data, partition + 1, end);
+}
 
-    int i = 0, j = 0, k = left;
-    
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            ++i;
-        } else {
-            arr[k] = R[j];
-            ++j;
+int SegregateStrings(string data[], int start, int end) {
+   int low;
+   int high;
+   int mid;
+   string pivot;
+   string temp;
+   bool sorted;
+   
+   mid = start + (end - start) / 2;
+   pivot = data[mid];
+   
+   sorted = false;
+   low = start;
+   high = end;
+   
+   while (!sorted) {
+      while (data[low].compare(pivot) < 0) {
+         ++low;
+      }
+      
+      while (pivot.compare(data[high]) < 0) {
+         --high;
+      }
+      
+      if (low >= high) {
+         sorted = true;
+      }
+      else {
+         temp = data[low];
+         data[low] = data[high];
+         data[high] = temp;
+         
+         ++low;
+         --high;
+      }
+   }
+   
+   return high;
+}
+
+void SortStrings(string data[], int start, int end) {
+   int partition;
+   
+   if (start >= end) {
+      return;
+   }
+   
+   partition = SegregateStrings(data, start, end);
+   
+   SortStrings(data, start, partition);
+   SortStrings(data, partition + 1, end);
+}
+
+bool BinarySearchInt(int arr[], int size, int key){
+    int begin = 0;
+    int finish = size;
+    while(begin <= finish){
+        int middle = (finish + begin)/2;
+        if(arr[middle] == key){
+            return true;
         }
-        ++k;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        ++i;
-        ++k;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        ++j;
-        ++k;
-    }
-}
-
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
-    }
-}
-
-// Quicksort algorithm for strings
-int partition(vector<string>& arr, int low, int high) {
-    string pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; ++j) {
-        if (arr[j] <= pivot) {
-            ++i;
-            swap(arr[i], arr[j]);
+        else if(arr[middle] > key){
+            finish = middle - 1;
+        }
+        else {
+            begin = middle + 1;
         }
     }
-    swap(arr[i + 1], arr[high]);
-    return i + 1;
+    return false;
 }
 
-void quickSort(vector<string>& arr, int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+bool BinarySearchString(string arr[], int size, string key){
+    int begin = 0;
+    int finish = size;
+    while(begin <= finish){
+        int middle = (finish + begin)/2;
+        if(arr[middle] == key){
+            return true;
+        }
+        else if(arr[middle] > key){
+            finish = middle - 1;
+        }
+        else {
+            begin = middle + 1;
+        }
     }
+    return false;
 }
 
 int main(int argc, char* argv[]) {
+    string option;
+    string filename1;
+    string filename2;
+    fstream file1;
+    fstream file2;
+    int numLines1 = 0;
+    int numLines2 = 0;
+
     if (argc != 4) {
-        cerr << "Usage: " << argv[0] << " <i|s> <input_file1> <input_file2>\n";
+        cout << "Syntax: ./prog (\"i\", \"s\") file1.txt file2.txt" << endl;
         return 1;
     }
 
-    string mode = argv[1];
-    string file1 = argv[2];
-    string file2 = argv[3];
+    option = argv[1];
+    filename1 = argv[2];
+    filename2 = argv[3];
 
-    ifstream infile1(file1);
-    ifstream infile2(file2);
+    file1.open(filename1);
+    if(file1.is_open()){
+        string line;
+        while(getline(file1, line)){
+            numLines1++;
+        }
+        file1.close();
+    }
 
-    if (!infile1 || !infile2) {
-        cerr << "Error: Unable to open input file.\n";
+    else{
+        cout << "Failed to open \"" << filename1 << "\"" << endl;
         return 1;
     }
 
-    vector<string> strings;
-    vector<int> integers;
-    string item;
-
-    // Read and store data from the first file
-    while (infile1 >> item) {
-        if (mode == "s")
-            strings.push_back(item);
-        else if (mode == "i")
-            integers.push_back(stoi(item));
+    file2.open(filename2);
+    if(file2.is_open()){
+        string line;
+        while(getline(file2, line)){
+            numLines2++;
+        }
+        file2.close();
     }
 
-    // Read and store data from the second file
-    while (infile2 >> item) {
-        if (mode == "s")
-            strings.push_back(item);
-        else if (mode == "i")
-            integers.push_back(stoi(item));
+    else{
+        cout << "Failed to open \"" << filename2 << "\"" << endl;
+        return 1;
+    }
+    
+    if(option == "i"){
+        int array1[numLines1];
+        int array2[numLines2];
+        int sameArray[numLines1];
+        string line1;
+        string line2;
+        int count1 = 0;
+        int count2 = 0;
+        int sameCount = 0;
+        file1.open(filename1);
+        file2.open(filename2);
+
+        while(getline(file1, line1)){
+            array1[count1] = stoi(line1);
+            count1++;
+        }
+
+        SortArray(array1, 0, count1-1);
+
+        int size = numLines1-1;
+        while(getline(file2, line2)){
+            array2[count2] = stoi(line2);
+            if (BinarySearchInt(array1, size, array2[count2])){
+                sameArray[sameCount] = stoi(line2);
+                sameCount++;
+            }
+            count2++;
+        }
+
+        SortArray(array2, 0, count2-1);
+
+        SortArray(sameArray, 0, sameCount-1);
+
+        for(int i = 0; i < sameCount; i++){
+
+            if(i > 0 && sameArray[i] == sameArray[i-1]){
+                continue;
+            }
+            else {
+                cout << sameArray[i] << endl;
+            }
+        }
     }
 
-    infile1.close();
-    infile2.close();
+    else if(option == "s"){       
+        string array1[numLines1];
+        string array2[numLines2];
+        string sameArray[numLines1];
+        string line1;
+        string line2;
+        int count1 = 0;
+        int count2 = 0;
+        int sameCount = 0;
+        file1.open(filename1);
+        file2.open(filename2);
 
-    // Sort data
-    if (mode == "s")
-        quickSort(strings, 0, strings.size() - 1);
-    else if (mode == "i")
-        mergeSort(integers, 0, integers.size() - 1);
+        while(getline(file1, line1)){
+            array1[count1] = line1;
+            count1++;
+        }
 
-    // Remove duplicates
-    if (mode == "s")
-        strings.erase(unique(strings.begin(), strings.end()), strings.end());
-    else if (mode == "i")
-        integers.erase(unique(integers.begin(), integers.end()), integers.end());
+        SortStrings(array1, 0, count1-1);
 
-    // Output sorted and unique values
-    if (mode == "s") {
-        for (const auto& str : strings)
-            cout << str << '\n';
-    } else if (mode == "i") {
-        for (const auto& num : integers)
-            cout << num << '\n';
+        int size = numLines1-1;
+
+        while(getline(file2, line2)){
+            array2[count2] = line2;
+            if (BinarySearchString(array1, size, array2[count2])){
+                sameArray[sameCount] = line2;
+                sameCount++;
+            }
+            count2++;
+        }
+
+        SortStrings(array2, 0, count2-1);
+
+        SortStrings(sameArray, 0, sameCount-1);
+
+        for(int i = 0; i < sameCount; i++){
+
+            if(i > 0 && sameArray[i] == sameArray[i-1]){
+                continue;
+            }
+            else {
+                cout << sameArray[i] << endl;
+            }
+        }
+    }
+
+    else {
+        cout << "Syntax: ./prog (\"i\", \"s\") file1.txt file2.txt" << endl;
+        return 1;
     }
 
     return 0;
